7961e1d40685efd58ed60013d6e261889515e544
Antonomy <antonyyu@gmail.com>
2023-02-14 15:57:34 -0800

    updated contact and demo links

--- src/components/BlogPost1.js
+++ src/components/BlogPost1.js
@@ -4,7 +4,7 @@ export default function BlogPost1() {
             <article class="blog-post">
                 <time datetime="2020-11-12">11/12/20</time>
                 <h2 class="post-title">On the Street in Brooklyn</h2>
-                <img src="/blog-image-1.jpg" alt="Blog 1 Image" />
+                <img src="https://cdn.vox-cdn.com/thumbor/UMVZEoh56tiat-Xqo8kCh0ioyf4=/0x0:3024x4032/1200x900/filters:focal(1271x1775:1753x2257):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/70380033/otra.0.jpg" alt="Blog 1 Image" />
                 <p class="post-text">Cray ipsum, dolor sit amet consectetur adipisicing elit. Repellat quidem deserunt nemo
                     dignissimos fuga veniam inventore eligendi magnam aperiam odit mollitia cupiditate eveniet velit
                     officiis magni aliquid, laborum dolor? Quasi? Veniam amet rerum ducimus est ea at neque alias.
--- src/components/BlogPost2.js
+++ src/components/BlogPost2.js
@@ -4,7 +4,7 @@ export default function BlogPost2() {
             <article class="blog-post">
                 <time datetime="2020-11-11">11/11/20</time>
                 <h2 class="post-title">Vintage in Vogue</h2>
-                <img src="/blog-image-2.jpg" alt="Blog 2 Image" />
+                <img src="https://res.cloudinary.com/grubhub/image/upload/d_search:browse-images:default.jpg/w_300,q_auto:low,fl_lossy,dpr_2.0,c_fill,f_auto,h_300,g_auto/search/browse-images/pizza" alt="Blog 2 Image" />
                 <p class="post-text">Selfies sunt, dolor sit amet consectetur adipisicing elit. Repellat quidem deserunt
                     nemo dignissimos fuga veniam inventore eligendi magnam aperiam odit mollitia cupiditate eveniet velit
                     officiis magni aliquid, laborum dolor? Quasi? Veniam amet rerum ducimus est ea at neque alias.
